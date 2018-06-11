e439b7675cb296f0145155d132424d4a9e03d17e
Ann Schulte <aschulte1919@gmail.com>
2018-06-10 20:56:32 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var initialize = function () {
     content.style.display = "block";
     game.play();
     document.getElementById("remainingGuesses").textContent = game.guessesLeft;
-    document.getElementById("totalWins").textContent = this.wins;
+    document.getElementById("totalWins").textContent = game.wins;
 }
 
 document.getElementById("startBtn").onclick = initialize;
@@ -16,24 +16,27 @@ document.getElementById("startBtn").onclick = initialize;
 var game = {
 
     word: "",
-    words: ['ruby', 'rails', 'javascript', 'array', 'hash', 'sinatra', 'model', 'view', 'devise', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'data', 'design', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'database'],
+    words: ['ruby', 'rails', 'javascript', 'array', 'hash', 'sinatra', 'model', 'view', 'devise', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'function', 'google', 'amazon', 'data', 'design', 'gist', 'github', 'agile', 'route', 'gem'],
     lettersInWord: [],
     lettersGuess: [],
     displayWord: [],
     guessesLeft: 10,
     wins: 0,
     guess: "",
+    correctGuess: 0,
+    wordLength: 0,
 
     // Selects a random word from the word list sets the secret word
     randomWord: function () {
         this.word = this.words[Math.floor(Math.random() * this.words.length)];
         console.log(this.word);
+        this.wordLength = this.word.length;
+        console.log(this.wordLength);
     },
 
     // Seperates the letters of the random word selected
     setLettersInWord: function () {
         this.lettersInWord = this.word.split("");
-        console.log(this.lettersInWord);
     },
 
     // Displays the random word with __
@@ -55,35 +58,79 @@ var game = {
     handleGuess: function () {
         document.onkeyup = function (event) {
             game.guess = window.event.key;
-            console.log(game.guess);
             game.lettersGuess.push(game.guess);
             document.getElementById("guessedLetters").textContent = game.lettersGuess;
-            console.log(game.lettersInWord);
-            console.log(game.guess);
             if (game.lettersInWord.includes(game.guess)) {
                 for (var i = 0; i < game.word.length; i++) {
                     if (game.word[i] === game.guess) {
-                        game.displayWord[i] = " " + game.guess + " ";
+                        game.displayWord[i] = game.guess;
+                        game.correctGuess = game.correctGuess + 1;
+                        game.winner();
                     }
                 }
             } else {
-                document.getElementById("remainingGuesses").textContent = game.guessesLeft--;
+                game.guessesLeft--;
+                document.getElementById("remainingGuesses").textContent = game.guessesLeft;
+            }
+            if (game.guessesLeft == 0) {
+                document.getElementById("loser").style.display = "block";
             }
             game.updateDisplayWord(game.displayWord);
-            console.log(game.lettersGuess);
         }
     },
 
     // Displays the number of times the user has won
-    displayWins: function () {
-        if (this.lettersGuess === this.displayWord) {
-            document.getElementById("totalWins").textContent = game.wins++;
+    winner: function () {
+        if (game.correctGuess === game.wordLength) {
+            document.getElementById("winner").style.display = "block";
+            game.wins++;
+            document.getElementById("totalWins").textContent = game.wins;
         }
     },
 
-    //adds the letter 
+    //adds the letter
     updateDisplayWord: function (word) {
         document.getElementById("currentWord").textContent = word.join(" ");
+    },
+
+    //reset display
+    resetDisplay: function () {
+        document.getElementById("winner").style.display = "none";
+        document.getElementById("loser").style.display = "none";
+    },
+
+    //resets the letters the user has guess array
+    resetLetters: function () {
+        if (game.correctGuess === game.wordLength) {
+            game.lettersGuess = [];
+            document.getElementById("guessedLetters").textContent = game.lettersGuess;
+        }
+    },
+
+    //reset guesses left function
+    resetGuessLeft: function () {
+        if (game.correctGuess === game.wordLength) {
+            game.guessesLeft = 10;
+            document.getElementById("remainingGuesses").textContent = game.guessesLeft;
+        }
+    },
+
+    resetWord: function () {
+        if (game.correctGuess === game.wordLength) {
+            game.guess = "";
+            game.correctGuess = 0;
+            game.wordLength = 0;
+        }
+    },
+
+    //resets game when loser hits the play again button
+    reset: function () {
+        game.updateDisplayWord(game.displayWord);
+        game.resetGuessLeft();
+        game.resetLetters();
+        game.play();
+        game.resetDisplay();
+        game.resetWord();
     },
 
     // Game execution occurs
@@ -91,7 +138,9 @@ var game = {
         game.randomWord();
         game.setLettersInWord();
         game.displayCurrentWord();
-        game.displayWins();
         game.handleGuess();
     }
 };
+
+document.getElementById("plyAgainBtn").onclick = game.reset;
+document.getElementById("plyBtn").onclick = game.reset;
